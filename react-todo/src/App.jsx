46416576d45077dc63b4045695acb243a8044293
Gatera Prince marvin <gateraprincemarvin@gmail.com>
2025-12-14 21:43:17 +0200

    Delete react-todo directory
    Checks for the implementation of the testing component

--- react-todo/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import TodoList from './components/TodoList'
-
-export default function App() {
-  return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', padding: 24 }}>
-      <TodoList />
-    </div>
-  )
-}
